06ed7e5dcc48ee1cf8ab3d451214dedb33449fb5
ajaykumar2pp <ajay2kumarpp@gmail.com>
2025-01-16 09:18:22 +0530

    portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-3xl font-bold text-center text-blue-600'>My Portfolio</h1>
+      
+    </>
+  )
+}
+
+export default App
